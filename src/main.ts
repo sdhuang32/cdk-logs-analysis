0115f17a6dad9d92cb0dc1ac50242251c9f16e09
cwyu57 <cwyu57@gmail.com>
2021-07-24 15:38:35 +0800

    refact: reneame to CdkLogsAnalysisStack

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 
-export class MyStack extends cdk.Stack {
+export class CdkLogsAnalysisStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: cdk.StackProps = {}) {
     super(scope, id, props);
 
@@ -8,15 +8,13 @@ export class MyStack extends cdk.Stack {
   }
 }
 
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
-
 const app = new cdk.App();
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
-// new MyStack(app, 'my-stack-prod', { env: prodEnv });
+new CdkLogsAnalysisStack(app, 'CdkLogsAnalysisStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
 
 app.synth();
