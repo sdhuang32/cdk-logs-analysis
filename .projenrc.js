b57cbda17901c1da945334c72b829efc22910879
cwyu57 <cwyu57@gmail.com>
2021-07-24 17:26:50 +0800

    chore: update projen dependency gitignore and tsconfig.eslint

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,15 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.114.0',
+  cdkVersion: '1.115.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-logs-analysis',
   cdkDependencies: [
+    '@aws-cdk/aws-cloudfront',
+    '@aws-cdk/aws-cloudfront-origins',
     '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-ecr-assets',
+    '@aws-cdk/aws-ecs',
     '@aws-cdk/aws-elasticloadbalancingv2',
-    '@aws-cdk/aws-s3',
   ],
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
@@ -15,4 +18,8 @@ const project = new AwsCdkTypeScriptApp({
   // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
   // release: undefined,                /* Add release management to this project. */
 });
+
+project.gitignore.exclude(...['cdk.context.json']);
+project.tsconfigEslint.addInclude('assets/**/*.ts');
+
 project.synth();
